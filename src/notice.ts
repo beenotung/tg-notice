2a5f42d044f78df9bc15df81e2ac3f9de0546643
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-03-16 12:04:50 +0800

    fixed tslint dep and stynax, updated license

--- src/notice.ts
+++ src/notice.ts
@@ -1,16 +1,16 @@
 import * as path from "path";
+import notifier = require("node-notifier");
 
-const notifier = require('node-notifier');
-const icon_path = path.join(__dirname + '/../res/icon.jpg');
+const icon_path = path.join(__dirname + "/../res/icon.jpg");
 // const icon_path = 'http://127.0.0.1:8080/ipfs/QmQhUFNnrk1XxFARWMUVnmQ5jW2bdPQmToFgP8QV5uKtM1';
 
 notifier.notify({
-  title: 'Gena Notice'
-  , message: 'Ready'
+  title: "Gena Notice"
+  , message: "Ready"
 });
 
-export function showNotice(message: string, title = 'Message from Gena', icon = true) {
-  console.log('showNotice', {message, title, icon});
+export function showNotice(message: string, title = "Message from Gena", icon = true) {
+  console.log("showNotice", {message, title, icon});
   return notifier.notify(Object.assign({
     title
     , message
--- src/telegram.ts
+++ src/telegram.ts
@@ -3,13 +3,13 @@ import {showNotice} from "./notice";
 import {str_contains} from "./utils";
 
 export function startTelegram() {
-  const child = spawn('telegram-cli');
+  const child = spawn("telegram-cli");
   let ss: string[] = [];
-  child.stdout.on('data', (buffer: Buffer) => {
+  child.stdout.on("data", (buffer: Buffer) => {
     const s = buffer.toString();
     ss.push(s);
-    if (s.indexOf('\n') !== -1) {
-      ss = ss.join('').split('\n');
+    if (s.indexOf("\n") !== -1) {
+      ss = ss.join("").split("\n");
       const last = ss.pop();
       ss.forEach(line => onLine(line));
       ss = [last];
@@ -20,22 +20,21 @@ export function startTelegram() {
 }
 
 function onLine(line: string) {
-  if (!str_contains('Gena', line)) {
+  if (!str_contains("Gena", line)) {
     return;
   }
   console.log(line);
-  if (str_contains(' <<< ', line)) {
+  if (str_contains(" <<< ", line)) {
     /* message from me */
-    console.log('[message from me]');
+    console.log("[message from me]");
     return;
   }
-  if (str_contains(' >>> ', line)) {
+  if (str_contains(" >>> ", line)) {
     /* message from gena */
-    console.log('[message from Gena]');
-    const ss = line.split(' >>> ');
+    console.log("[message from Gena]");
+    const ss = line.split(" >>> ");
     ss.shift();
-    const message = ss.join('Gena >>> ').replace('\u001b[0m', '');
+    const message = ss.join("Gena >>> ").replace("\u001b[0m", "");
     showNotice(message);
   }
 }
-
