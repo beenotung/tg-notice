944ee509c0bf381ed2cfd75f1867461f3f606a2a
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-12-16 12:34:45 +0800

    updated for latest telegram-cli formatting

--- src/telegram.ts
+++ src/telegram.ts
@@ -1,6 +1,6 @@
 import {spawn} from "child_process";
 import {showNotice} from "./notice";
-import {str_contain_any, str_contains} from "./utils";
+import {compare_number, str_contain_any} from "./utils";
 import {whiteList, whiteNames} from "./config";
 
 export function startTelegram() {
@@ -24,34 +24,101 @@ export function startTelegram() {
   child.stderr.pipe(process.stderr);
 }
 
+/* send */
+const left_arrows = "«<";
+/* receive */
+const right_arrows = "»>";
+
+function checkArrow(line: string, arrows: string) {
+  for (const a of arrows) {
+    const pattern = " " + a.repeat(3) + " ";
+    if (line.includes(pattern)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function isReceivingMessage(line: string): boolean {
+  return checkArrow(line, right_arrows);
+}
+
+function isSendingMessage(line: string): boolean {
+  return checkArrow(line, left_arrows);
+}
+
+function getArrowChar(line: string): string {
+  const cs = [];
+  cs.push(...left_arrows.split(""));
+  cs.push(...right_arrows.split(""));
+  const xs = cs
+    .map(c => ({
+      c,
+      idx: line.indexOf((c as string).repeat(3))
+    }))
+    .filter(x => x.idx !== -1);
+  xs.sort((a, b) => compare_number(a.idx, b.idx));
+  return xs[0].c;
+}
+
+function getArrowPattern(line: string): string {
+  return " " + getArrowChar(line).repeat(3) + " ";
+}
+
+type MessageMode = "receive" | "send";
+
+interface Message {
+  sender: string
+  message: string
+  mode: MessageMode
+}
+
+function extractMessage(line: string): Message {
+  const arrow_pattern = getArrowPattern(line);
+  const ss = line.split(arrow_pattern);
+  let sender = ss.shift().split("]")[1].trim();
+  /* remove unicode sequence that control coloring */
+  sender = sender.substring(19, sender.length - 18);
+  const message = ss.join(arrow_pattern).replace("\u001b[0m", "");
+  let mode: MessageMode;
+  if (isReceivingMessage(line)) {
+    mode = "receive";
+  } else if (isSendingMessage(line)) {
+    mode = "send";
+  }
+  return {
+    mode,
+    sender,
+    message,
+  };
+}
+
 function onLine(line: string) {
-  if (!str_contain_any(whiteNames, line)) {
+  console.debug(line);
+  if (!(isSendingMessage(line) || isReceivingMessage(line))) {
+    /* system message */
     return;
   }
-  console.log(line);
-  if (str_contains(" <<< ", line)) {
+  const msg = extractMessage(line);
+  console.debug("message:", msg);
+  if (!str_contain_any(whiteNames, msg.sender)) {
+    return;
+  }
+  if (msg.mode === "send") {
     /* message from myself */
     console.log("[message from myself]");
     return;
   }
-  if (str_contains(" >>> ", line)) {
+  if (msg.mode === "receive") {
     /* message from whitelist */
-
-    const group_and_sender = line
-      .split(" <<< ")[0]
-      .split("]")[1]
-      .trim();
-    const sender = whiteNames.find(p => str_contains(p, group_and_sender));
+    const sender = msg.sender;
     if (sender === undefined) {
       console.error("sender is not found on the line:", line);
       return;
     }
-    const sender_icon = whiteList.filter(x => x.name === sender)[0].icon;
-
     console.log(`[message from ${sender}]`);
-    const ss = line.split(" >>> ");
-    ss.shift();
-    const message = ss.join(" >>> ").replace("\u001b[0m", "");
+    const sender_icon = whiteList.filter(x => x.name === sender)[0].icon;
+    const message = msg.message;
     showNotice(message, sender + ":", sender_icon);
     return;
   }
--- src/utils.ts
+++ src/utils.ts
@@ -5,3 +5,7 @@ export function str_contains(pattern: string, target: string) {
 export function str_contain_any(patterns: string[], target: string) {
   return patterns.some(p => str_contains(p, target));
 }
+
+export function compare_number(a: number, b: number): 1 | 0 | -1 {
+  return a < b ? -1 : (a > b ? 1 : 0);
+}
